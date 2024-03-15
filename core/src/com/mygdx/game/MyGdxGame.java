fe40ed387e64be31d118d24b1df2a4e0d1fc5f9c
Ralphato <160437493+Ralphato@users.noreply.github.com>
2024-03-15 18:02:18 -0500

    Update MyGdxGame.java
    Uncompleted (needs some cleaning too)

--- core/src/com/mygdx/game/MyGdxGame.java
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -27,7 +27,8 @@ public class MyGdxGame extends ApplicationAdapter {
     private Music f_BGM;
     private ArrayList<Platform> f_platforms;
     private Platform f_currentPlatform; 
-    private Rectangle f_playerBounds;
+    private float PlatformY;
+    private Rectangle f_playerBounds, f_legBounds, f_bodyBounds;
     private boolean f_onPlatform, f_contactPlatform;
 
     /**
@@ -45,7 +46,8 @@ public class MyGdxGame extends ApplicationAdapter {
         f_BGM.play();
         f_BGM.setLooping(true);
         f_platforms = new ArrayList<>();
-        f_playerBounds = f_player.getBounds();
+        f_playerBounds = f_player.getBodyBounds();
+        f_legBounds = f_player.getLegsBounds();
         
         initializePlatforms();
     }
@@ -79,7 +81,11 @@ public class MyGdxGame extends ApplicationAdapter {
     public void render() {
         clearScreen();
         f_player.update(Gdx.graphics.getDeltaTime());
-        f_playerBounds = f_player.getBounds();
+        //render bounds
+        f_playerBounds = f_player.getBodyBounds();
+        f_legBounds = f_player.getLegsBounds();
+        f_bodyBounds = f_player.getBodyBounds();
+        
         f_batch.begin();
         renderPlatforms();
         f_player.draw(f_batch);
@@ -91,12 +97,19 @@ public class MyGdxGame extends ApplicationAdapter {
         shapeRenderer.begin(ShapeRenderer.ShapeType.Line);
         shapeRenderer.setColor(Color.RED);
         for (Platform platform : f_platforms) {
+            Rectangle Upperbounds = platform.getUpperBounds();
+            shapeRenderer.rect(Upperbounds.x, Upperbounds.y, Upperbounds.width, Upperbounds.height);
             Rectangle bounds = platform.getBounds();
             shapeRenderer.rect(bounds.x, bounds.y, bounds.width, bounds.height);
         }
+     
         // Draw player bounds
-        Rectangle playerBounds = f_player.getBounds();
+        Rectangle playerBounds = f_player.getBodyBounds();
         shapeRenderer.rect(playerBounds.x, playerBounds.y, playerBounds.width, playerBounds.height);
+        shapeRenderer.setColor(Color.GREEN);
+        Rectangle playerBoundsLegs = f_player.getLegsBounds();
+        shapeRenderer.rect(playerBoundsLegs.x, playerBoundsLegs.y, playerBoundsLegs.width, playerBoundsLegs.height);
+        
         shapeRenderer.end();
 
     }
@@ -122,19 +135,57 @@ public class MyGdxGame extends ApplicationAdapter {
      * Checks for collisions between the player and platforms.
      */
     private void checkPlayerPlatformCollisions() {
-    	f_contactPlatform = false;
+        boolean foundCollision = false;
+        // Start with the assumption that there is no collision
         for (Platform platform : f_platforms) {
-            if (f_playerBounds.overlaps(platform.getBounds())) {
-            	f_currentPlatform = platform;
-            	f_player.setCurrentPlatform(f_currentPlatform);
+            if (f_legBounds.overlaps(platform.getUpperBounds())) {
+            	//System.out.println("Collision detected");
+                f_currentPlatform = platform;
+                f_player.setCurrentPlatform(f_currentPlatform);
+                PlatformY = f_currentPlatform.getPlatY();
+                f_player.checkCollision(foundCollision);
                 resolvePlayerPlatformCollision(platform);
-                f_contactPlatform = true;
-                
-            }
-            if (!f_contactPlatform) {
-            	f_currentPlatform = null;
+                foundCollision = true;
+                break; // This stops checking more platforms after finding the first collision
             }
         }
+        
+        
+        
+        // If after checking all platforms, no collision is found, then reset.
+        if (!foundCollision) {
+        	
+            //f_currentPlatform = null;
+            f_player.setCurrentPlatform(null);
+            f_player.checkCollision(foundCollision);
+            float y = f_player.getPositionY();
+            if(y >= PlatformY) {
+        	y += -5;
+        	f_player.setPosition(f_player.getPositionX(), y);
+        	}
+        }
+        
+        if(foundCollision) {
+        	float y = f_player.getPositionY();
+            if(y >= PlatformY) {
+        	y += -5;
+        	f_player.setPosition(f_player.getPositionX(), y);
+        }
+        }
+        
+        //Stop player when colliding with platform sides with its body
+        for(Platform platform : f_platforms) {
+        	if(f_bodyBounds.overlaps(platform.getBounds())) {
+        			System.out.println("Body is colliding with platform side");
+        			float newX = platform.getBounds().x - 10;
+        			f_player.setPosition(newX - 70, f_player.getPositionY()); //instead of newX put newX - f_player.width
+        			//f_player.stopMovement();
+        			
+        			
+        	
+          }
+        }
+        
     }
 
     /**
@@ -143,12 +194,11 @@ public class MyGdxGame extends ApplicationAdapter {
      * @param platform The platform that collides with the player.
      */
     private void resolvePlayerPlatformCollision(Platform platform) {
-        // Collision resolution logic...
-    	float newY = platform.getBounds().y + platform.getBounds().height; // Position player on top of the platform
-        f_player.setPosition(f_player.getPositionX(), newY); // Assuming a setPosition or similar method in Player
-    	
+        float newY = platform.getBounds().y  + platform.getBounds().height; // Subtract a small value to ensure overlap
+        f_player.setPosition(f_player.getPositionX(), newY);
     }
-
+    
+  
     /**
      * Disposes game resources.
      */
